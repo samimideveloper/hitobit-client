4bd3a3950768aebd053d4b3418ee2e07d6897655
Hossein Mohammadi <hosseinm.developer@gmail.com>
2022-07-31 13:41:05 +0430

    fix: export asyncStorage

--- src/hitobit-client/src/index.ts
+++ src/hitobit-client/src/index.ts
@@ -6,6 +6,11 @@ export {
   TWENTY_SECOND,
 } from "hitobit-constants";
 export * from "hitobit-hooks";
-export { cookieStorage, getDeviceId, URLManager } from "hitobit-modules";
+export {
+  AsyncStorage,
+  cookieStorage,
+  getDeviceId,
+  URLManager,
+} from "hitobit-modules";
 export * from "hitobit-store";
 export * from "hitobit-utils";
