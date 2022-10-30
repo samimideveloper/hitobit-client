2a37f8bf7b59f307cde63ef74c6295d13dc54e17
Mohammad Gharouni <mohammadgharouni@icloud.com>
2022-10-30 16:54:50 +0330

    added max quantity to useStepValues

--- src/hooks/useStepValues/index.tsx
+++ src/hooks/useStepValues/index.tsx
@@ -1,8 +1,16 @@
+import Decimal from "decimal.js";
 import starkString from "starkstring";
+import { useMarketTicker } from "../marketTicker";
+import { useMarketFilters } from "../useMarketFilters";
 import { useStepSize } from "../useStepSize";
 
 export const useStepValues = (symbol?: string) => {
   const { toStepSize } = useStepSize(symbol);
+  const { getSymbolMarketTicker } = useMarketTicker();
+  const selectedMarket = getSymbolMarketTicker(symbol);
+  const { maxQuantity } = useMarketFilters({
+    selectedMarket,
+  });
 
   const expectedValue = (value?: string | null) =>
     starkString(value || "")
@@ -13,6 +21,10 @@ export const useStepValues = (symbol?: string) => {
     const _numberedValue = starkString(_value || "")
       .parseNumber({ decimal: true })
       .toNumber();
+
+    if (new Decimal(_numberedValue).greaterThan(maxQuantity)) {
+      return maxQuantity;
+    }
 
     const _numberedStepValue = Number(
       toStepSize(
