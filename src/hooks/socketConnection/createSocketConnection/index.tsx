d7bfa31b55ae4252940b12119d9f59e4d4b7a22f
a.hemati <alirezahematideveloper71@gmail.com>
2023-01-21 12:46:56 +0330

    fix test 2

--- src/hooks/socketConnection/createSocketConnection/index.tsx
+++ src/hooks/socketConnection/createSocketConnection/index.tsx
@@ -40,6 +40,11 @@ export const createSocketConnection = <T extends string>(
         <SocketConnection.Provider
           url={URLManager.signalRBaseUrl}
           onOpen={async (connection) => {
+            if (connection.state === HubConnectionState.Disconnected) {
+              await connection.start();
+            }
+            await connection?.invoke("UNSUBSCRIBE", [...events]);
+
             if (connection?.state === HubConnectionState.Connected) {
               if (__DEV__) {
                 connection.keepAliveIntervalInMilliseconds = 120000;
@@ -59,6 +64,11 @@ export const createSocketConnection = <T extends string>(
             }
           }}
           onReconnect={async (connection) => {
+            if (connection.state === HubConnectionState.Disconnected) {
+              await connection.start();
+            }
+            await connection?.invoke("UNSUBSCRIBE", [...events]);
+
             if (connection?.state === HubConnectionState.Connected) {
               await connection?.invoke("SUBSCRIBE", [
                 ...events,
